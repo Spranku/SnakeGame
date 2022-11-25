12d394b59b9b69201062997401ab64e13d33eb80
Spranky <spranky.press@yandex.ru>
2022-11-25 21:22:09 +0300

    Build v2.0

---
+++ Source/SnakeGame/BonusFood.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Food.h"
+#include "BonusFood.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAKEGAME_API ABonusFood : public AFood
+{
+	GENERATED_BODY()
+	
+};
